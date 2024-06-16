ad486bf954b2249bf683e34c824195b90d87d43a
mahad002 <mahad112002@gmail.com>
2024-06-16 22:39:58 +0500

    .

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -16,7 +16,7 @@ export const navData = [
   { name: "home", path: "/", Icon: HiHome },
   { name: "about", path: "/about", Icon: HiUser },
   { name: "services", path: "/services", Icon: HiRectangleGroup },
-  { name: "work", path: "/work", Icon: HiViewColumns },
+  { name: "drive", path: "/work", Icon: HiViewColumns },
   {
     name: "Motivation",
     path: "/testimonials",
